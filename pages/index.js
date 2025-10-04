29ef67dd34d0222a774b377797ba3bb10d890888
Ramon Oliveira <ramon94cardoso@gmail.com>
2025-10-04 18:28:48 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// default React usage for components, they must follow the "Pattern"
+function Home() {
+    return <h1>Se você gosta de VP Pizza, dá uma risadinha!</h1>
+}
+
+// exporting the function so Next knows what to look for and render it properly
+export default Home;
