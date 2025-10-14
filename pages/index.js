9934284cba423a773c7499cf34c4e640dcaf4777
Ramon Oliveira <ramon94cardoso@gmail.com>
2025-10-14 20:15:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // default React usage for components, they must follow the "Pattern"
 function Home() {
-    return <h1>Se você gosta de VP Pizza, dá uma risadinha! 😁</h1>
+  return <h1>Se você gosta de VP Pizza, dá uma risadinha! 😁</h1>;
 }
 
 // exporting the function so Next knows what to look for and render it properly
